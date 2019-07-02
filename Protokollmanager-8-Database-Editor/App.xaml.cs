1192ec3447f0765c318916984be12a86b0216111
Maximilian Hildebrand <maximilian.hildebrand@gmx.de>
2019-07-02 11:09:26 +0200

    Initial commit

---
+++ Protokollmanager-8-Database-Editor/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Protokollmanager_8_Database_Editor
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
